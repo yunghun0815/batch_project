847aff20b8412aa54b9fb7b86723299ab14d2b11
정영훈 <yunghun0815@gmail.com>
2022-12-13 11:41:28 +0900

    init

---
+++ batch_project/src/main/java/com/kosa/batch/BatchProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kosa.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchProjectApplication.class, args);
+	}
+
+}
---
+++ batch_project/src/test/java/com/kosa/batch/BatchProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosa.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
