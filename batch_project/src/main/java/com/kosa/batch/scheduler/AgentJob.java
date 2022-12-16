b0403254fecb95d8caf8350f9672c7e34490baf2
정영훈 <yunghun0815@gmail.com>
2022-12-16 11:57:20 +0900

    Merge branch 'lsh' of https://github.com/yunghun0815/batch_project.git into jyh

--- batch_project/src/main/java/com/kosa/batch/scheduler/AgentJob.java
+++ batch_project/src/main/java/com/kosa/batch/scheduler/AgentJob.java
@@ -2,8 +2,6 @@ package com.kosa.batch.scheduler;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 import org.json.simple.JSONObject;
@@ -16,50 +14,60 @@ import org.springframework.stereotype.Component;
 
 import com.kosa.batch.model.BatchGroupVo;
 import com.kosa.batch.service.IBatchService;
-import com.kosa.batch.service.impl.BatchService;
+import com.kosa.batch.socket.ExecutorServiceEx;
 
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
 public class AgentJob implements Job{
-	
+
 	@Autowired
 	IBatchService batchService;
-	
+
+	@Autowired
+	ExecutorServiceEx executorServiceEx;
+
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
-		try {
-			//잡키로 찾기 path 여러개 + 아이피 + 포트
-			log.info("키" + context.getJobDetail().getKey());
-			List<BatchGroupVo> batchGroupVo = batchService.getBatchGroupByJobKey(context.getJobDetail().getKey().toString());
-			
-			log.info("");
-			log.info("-----------------------------------");
-			log.info(batchGroupVo.get(0).getIp()+":"+batchGroupVo.get(0).getPort()+" 배치 프로그램 실행");
-			String result = null;
-			
-			//배치그룹에 등록된 앱 수만큼 실행
-			for(BatchGroupVo vo : batchGroupVo) {
-				Process process = Runtime.getRuntime().exec(vo.getPath());
-				BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
-				
-				JSONParser jsonParser = new JSONParser();
-				JSONObject jsonObject = new JSONObject();
-						
-				while((result=br.readLine()) != null) {
-					jsonObject = (JSONObject) jsonParser.parse(result);
-					log.info("배치 완료 시간 : " + jsonObject.get("date"));
-					log.info("실행 결과 : " + jsonObject.get("result"));
-				}
-			}
-				
-			log.info("-----------------------------------");
+		//잡키로 찾기 path 여러개 + 아이피 + 포트
+		log.info("키" + context.getJobDetail().getKey());
+		List<BatchGroupVo> batchGroupVo = batchService.getBatchGroupByJobKey(context.getJobDetail().getKey().toString());
 
-		} catch (Exception e) {
-			System.err.println(e);
+		log.info("");
+		log.info("-----------------------------------");
+		log.info(batchGroupVo.get(0).getIp()+":"+batchGroupVo.get(0).getPort()+" 배치 프로그램 실행");
+
+		//배치그룹에 등록된 앱 수만큼 실행
+		for(BatchGroupVo vo : batchGroupVo) {
+			executorServiceEx.putTask(new Runnable() {
+				@Override
+				public void run() {
+
+					try {
+						log.info("시작");
+						String result = null;
+						Process process = Runtime.getRuntime().exec(vo.getPath());
+						BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+						JSONParser jsonParser = new JSONParser();
+						JSONObject jsonObject = new JSONObject();
+
+						while((result=br.readLine()) != null) {
+							jsonObject = (JSONObject) jsonParser.parse(result);
+							log.info("배치 완료 시간 : " + jsonObject.get("date"));
+							log.info("실행 결과 : " + jsonObject.get("result"));
+						}	
+					}catch(Exception e) {
+						log.info(e.getMessage());
+						e.printStackTrace();
+					}
+				}
+			});
 		}
+
+		log.info("-----------------------------------");
+
 	}
 
 }
